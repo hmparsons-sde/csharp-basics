a2b8ab21cf12e3eb987101735973031e06d0bbc5
Nathan Gonzalez <ncgonz@gmail.com>
2021-01-16 13:02:24 -0600

    class from 1/12/2020

--- MyFirstConsoleApplication/Program.cs
+++ MyFirstConsoleApplication/Program.cs
@@ -6,10 +6,25 @@ namespace MyFirstConsoleApplication
     {
         static void Main(string[] args)
         {
+            //read input from a user
+            Console.WriteLine("What is your first name");
+            var input = Console.ReadLine();
+
+            //strings
             string name = "Nathan";
             char firstLetter = 'N';
 
+            //let myInterpolatedString =  `this stuff ${firstLetter}`;
+            var myInterpolatedString = $"My name is {input} and the " +
+                                       $"first letter of my name is {firstLetter}";
+
+            var stringLiteral = @"This is a paragraph.
+                                  It is across multiple lines.
+                                  Returns will be honored in the output.";
             
+            Console.WriteLine(myInterpolatedString);
+            Console.WriteLine(stringLiteral);
+
             //Number types
             int number = Int32.MaxValue;
 
@@ -20,6 +35,10 @@ namespace MyFirstConsoleApplication
             Console.WriteLine(number);
             Console.WriteLine(bigNumber);
             Console.WriteLine(littleNumber);
+
+            //decimals can be represented by [f]loat(32bit) [d]ouble(64bit) or deci[m]al(128bit) 
+            var adding = 1 + 1.1d;
+            var division = 3 / 2d;
 
 
             var implicitStringThing = "this is an implicitly typed variable.";
@@ -40,6 +59,52 @@ namespace MyFirstConsoleApplication
             weirdStuff = "More craziness";
 
             Console.WriteLine(weirdStuff);
+
+            var names = new string[5];
+
+            var secondName = names[1];
+            names[1] = "Jimmy";
+
+            var colors = new string[] {"white", "blue", "black", "yellow", "teal", "magenta"};
+
+
+            if (input == "Nathan")
+            {
+                Console.WriteLine("Duh");
+            }
+            else if (input == "blerg")
+            {
+                Console.WriteLine(input);
+            }
+            else
+            {
+                Console.WriteLine("Not Nathan");
+            }
+
+            for (var i = 0; i < colors.Length; i++)
+            {
+                Console.WriteLine($"currently for looping on color {colors[i]}");
+            }
+
+            var counter = 1;
+            while (true)
+            {
+                Console.WriteLine($"While loop iteration {counter}.");
+
+                if (counter <= 5)
+                {
+                    counter++;
+                    continue;
+                }
+
+                break;
+            }
+
+            foreach (var color in colors)
+            {
+                Console.WriteLine($"Foreach looping on color {color}");
+            }
+
 
         }
     }
